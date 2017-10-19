823d722620c2df90fa32e7401995a18480b09db1
justas <jkavaliauskas@gmail.com>
2017-10-19 11:44:03 +0300

    Initial spring security

--- src/main/java/lt/codeacademy/controllers/HomeController.java
+++ src/main/java/lt/codeacademy/controllers/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.security.Principal;
 
 @RestController
